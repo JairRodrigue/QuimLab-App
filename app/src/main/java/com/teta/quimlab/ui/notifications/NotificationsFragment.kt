23ceb15cea82ee49471713dd6e054fed3fc5097f
jairodrig <jair.rodrigues@ufrpe.br>
2024-09-28 15:27:41 -0300

    Tela configuração iniciada

--- app/src/main/java/com/teta/quimlab/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/teta/quimlab/ui/notifications/NotificationsFragment.kt
@@ -1,20 +1,21 @@
 package com.teta.quimlab.ui.notifications
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.teta.quimlab.PerfilActivity
+import com.teta.quimlab.R
 import com.teta.quimlab.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,10 +29,14 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+
+        // Configurar o botão para abrir a PerfilActivity
+        val buttonToProfile: TextView = binding.optionToProfile
+        buttonToProfile.setOnClickListener {
+            val intent = Intent(requireActivity(), PerfilActivity::class.java)
+            startActivity(intent)
         }
+
         return root
     }
 
